406e54b34a3a982e8f19e7e5876749f8462d41e9
Brian Stacy <bstacy@worldbank.org>
2019-08-16 09:24:15 -0400

    updates to data cleaner code

--- Master_Code/data_cleaner.R
+++ Master_Code/data_cleaner.R
@@ -13,6 +13,18 @@ library(tidyr)
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 setwd('..')
 
+#Create function to save metadata for each question in each module
+#The attr function retrieves metadata imported by haven. E.g. attr(school_dta$m1s0q2_code, "label")
+makeVlist <- function(dta) { 
+  varlabels <- sapply(dta, function(x) attr(x,"label"))
+  vallabels <- sapply(dta, function(x) attr(x,"labels"))
+  tibble(name = names(varlabels),
+         varlabel = varlabels, vallabel = vallabels) 
+}
+
+
+
+
 #Load the data
 
 ###########################
@@ -20,7 +32,19 @@ setwd('..')
 ###########################
 download_folder <- choose.dir(default = "", caption = "Select folder to open data downloaded from API")
 
-school_dta<-read_dta(file.path(download_folder, "EPDash.dta"))
+raw_school_dta<-read_dta(file.path(download_folder, "EPDash.dta"))
+
+#rename a few key variables up front
+raw_school_dta<- raw_school_dta %>%
+  mutate(enumerator_name=m1s0q1_name  ,
+            enumerator_number=m1s0q1_number ,
+            survey_time=m1s0q8,
+            lat=m1s0q9__Latitude,
+            lon=m1s0q9__Longitude
+            )
+
+#create school metadata frame
+school_metadta<-makeVlist(raw_school_dta)
 
 #Read in list of indicators
 indicators <- read_delim("./Indicators/indicators.md", delim="|", trim_ws=TRUE)
@@ -28,13 +52,22 @@ indicators <- indicators %>%
   filter(Series!="---") %>%
   separate(Series, c(NA, NA, "indicator_tag"), remove=FALSE)
 
+#Get list of indicator tags, so that we are able to select columns from our dataframe using these indicator tags that were also programmed into Survey Solutions
 indicator_names <- indicators$indicator_tag
-#use dplyr select(contains()) to search for variables with select tags and create separate databases
+
+#list additional info that will be useful to keep in each indicator dataframe
+preamble_info <- c('interview__id', 'school_name_preload', 'school_address_preload', 
+                   'school_province_preload', 'school_district_preload', 'school_code_preload', 'school_emis_preload',
+                   'enumerator_name', 'enumerator_number', 'survey_time', 'lat', 'lon')
+
+#use dplyr select(contains()) to search for variables with select tags and create separate databases by indicator
+#This will make the information for each indicator contained in an independent database
+#Will need to join the school level information with teacher level questionnaire information for some indicators.  This will be done later.
 for (i in indicator_names ) {
-  temp_df<-school_dta %>%
-    select(contains(i))
+  temp_df<-raw_school_dta %>%
+    select(preamble_info, contains(i))
   if (ncol(temp_df) > 0) {
-    assign(paste("school_data_",i, sep=""), temp_df )
+    assign(paste("raw_school_data_",i, sep=""), temp_df )
   }
 }
 
@@ -42,24 +75,52 @@ for (i in indicator_names ) {
 ########################
 #read in ecd level file
 ########################
-ecd_dta<-read_dta(file.path(download_folder, "ecd_assessment.dta"))
+raw_ecd_dta<-read_dta(file.path(download_folder, "ecd_assessment.dta"))
 
 ########################################
 #read in 4th grade assessment level file
 ########################################
-assess_4th_grade_dta<-read_dta(file.path(download_folder, "fourth_grade_assessment.dta"))
+raw_assess_4th_grade_dta<-read_dta(file.path(download_folder, "fourth_grade_assessment.dta"))
 
 #########################################
 #read in teacher questionnaire level file
 #########################################
-teacher_questionnaire<-read_dta(file.path(download_folder, "questionnaire_roster.dta"))
+raw_teacher_questionnaire<-read_dta(file.path(download_folder, "questionnaire_roster.dta"))
+raw_teacher_questionnaire_metadta<-makeVlist(raw_teacher_questionnaire)
+
+#rename a few key variables up front
+raw_teacher_questionnaire<- raw_teacher_questionnaire %>%
+  mutate(teacher_name=m3sb_troster  ,
+         teacher_number=m3sb_tnumber ,
+         available=m3s0q1,
+         teacher_position=m3saq1,
+         teacher_grd1=m3saq2__1==1,
+         teacher_grd2=m3saq2__2==1,
+         teacher_grd3=m3saq2__3==1,
+         teacher_grd4=m3saq2__4==1,
+         teacher_grd5=m3saq2__5==1,
+         teacher_language=m3saq3__1==1,
+         teacher_math=m3saq3__2==1,
+         teacher_both_subj=m3saq3__3==1,
+         teacher_other_subj=m3saq3__4==1,
+         teacher_education=m3saq4,
+         teacher_year_began=m3saq5,
+         teacher_age=m3saq6,
+  )
+
+#list additional info that will be useful to keep in each indicator dataframe
+preamble_info <- c('interview__id', 'questionnaire_roster_id', 'teacher_name', 'teacher_number',
+                   'available', 'teacher_position', 'teacher_grd1', 'teacher_grd2', 'teacher_grd3', 'teacher_grd4', 'teacher_grd5',
+                   'teacher_language', 'teacher_math', 'teacher_both_subj', 'teacher_other_subj', 'teacher_education', 'teacher_year_began',
+                   'teacher_age')
+
 
 #use dplyr select(contains()) to search for variables with select tags and create separate databases
 for (i in indicator_names ) {
-  temp_df<-teacher_questionnaire %>%
+  temp_df<-raw_teacher_questionnaire %>%
     select(contains(i))
   if (ncol(temp_df) > 0) {
-    assign(paste("teacher_questionnaire_",i, sep=""), temp_df )
+    assign(paste("raw_teacher_questionnaire_",i, sep=""), temp_df )
   }
 }
 
@@ -68,25 +129,26 @@ for (i in indicator_names ) {
 ################################
 #read in principal absence file
 ################################
-principal_absence_dta<-read_dta(file.path(download_folder, "principal_absence_roster2.dta"))
+raw_principal_absence_dta<-read_dta(file.path(download_folder, "principal_absence_roster2.dta"))
 
 
 #############################################
 ##### Teacher Absence ###########
 #############################################
 
-teacher_absence_dta<-read_dta(file.path(download_folder, "absence_roster2.dta"))
+raw_teacher_absence_dta<-read_dta(file.path(download_folder, "absence_roster2.dta"))
+
 
 
 #create indicator for whether each teacher was absent from school
-teacher_absence_dta <- teacher_absence_dta %>%
+raw_teacher_absence_dta <- raw_teacher_absence_dta %>%
   mutate(school_absent=case_when(
     m2sbq6==6  ~ 1,
     m2sbq6!=6   ~ 0,
     is.na(m2sbq6) ~ as.numeric(NA)))
 
 #create indicator for whether each teacher was absent from classroom or school
-teacher_absence_dta <- teacher_absence_dta %>%
+raw_teacher_absence_dta <- raw_teacher_absence_dta %>%
   mutate(absent=case_when(
     m2sbq6==6 | m2sbq6==5 | m2sbq6==2  ~ 1,
     m2sbq6!="Absent" & m2sbq6!=5 &  m2sbq6!=2 ~ 0,
@@ -94,7 +156,7 @@ teacher_absence_dta <- teacher_absence_dta %>%
 
 
 #Build teacher absence practice indicator
-school_absence_rate <- teacher_absence_dta %>%
+final_school_absence_rate <- raw_teacher_absence_dta %>%
   group_by(interview__id) %>%
   summarise(school_absence_rate=mean(school_absent), absence_rate=mean(absent))
 
@@ -103,7 +165,7 @@ school_absence_rate <- teacher_absence_dta %>%
 ##### Teacher Knowledge ###########
 #############################################
 
-teacher_assessment_dta<-read_dta(file.path(download_folder, "teacher_assessment.dta"))
+raw_teacher_assessment_dta<-read_dta(file.path(download_folder, "teacher_assessment.dta"))
 
 #create indicator for % correct on teacher assessment
 #Note:  in the future we could incorporate irt program like mirt
@@ -112,36 +174,36 @@ teacher_assessment_dta<-read_dta(file.path(download_folder, "teacher_assessment.
 ####Literacy####
 #calculate # of literacy items
 #note: grep is a tool for searching text with specified pattern.  In our case, we are looking for m5s1q, which is the prefix of literacy items
-teacher_assessment_dta$literacy_length<-length(grep(x=colnames(teacher_assessment_dta), pattern="m5s1q"))
+raw_teacher_assessment_dta$literacy_length<-length(grep(x=colnames(raw_teacher_assessment_dta), pattern="m5s1q"))
 
 ####NEED TO FIX THIS BECAUSE some items have non-binary responses####
 
-lit_items<-colnames(teacher_assessment_dta[,grep(x=colnames(teacher_assessment_dta), pattern="m5s1q")])
+lit_items<-colnames(raw_teacher_assessment_dta[,grep(x=colnames(raw_teacher_assessment_dta), pattern="m5s1q")])
 
 #calculate teachers lit items correct
-teacher_assessment_dta <- teacher_assessment_dta %>%
-  mutate(literacy_content_knowledge=rowSums(.[grep(x=colnames(teacher_assessment_dta), pattern="m5s1q")]))
+raw_teacher_assessment_dta <- raw_teacher_assessment_dta %>%
+  mutate(literacy_content_knowledge=rowSums(.[grep(x=colnames(raw_teacher_assessment_dta), pattern="m5s1q")]))
 
 ####Math####
 #calculate # of math items
-teacher_assessment_dta$math_length<-length(grep(x=colnames(teacher_assessment_dta), pattern="m5s2q"))
+raw_teacher_assessment_dta$math_length<-length(grep(x=colnames(raw_teacher_assessment_dta), pattern="m5s2q"))
 
-math_items<-colnames(teacher_assessment_dta[,grep(x=colnames(teacher_assessment_dta), pattern="m5s2q")])
+math_items<-colnames(raw_teacher_assessment_dta[,grep(x=colnames(raw_teacher_assessment_dta), pattern="m5s2q")])
 
 #calculate teachers math items correct
-teacher_assessment_dta <- teacher_assessment_dta %>%
-  mutate(math_content_knowledge=rowSums(.[grep(x=colnames(teacher_assessment_dta), pattern="m5s2q")]))
+raw_teacher_assessment_dta <- raw_teacher_assessment_dta %>%
+  mutate(math_content_knowledge=rowSums(.[grep(x=colnames(raw_teacher_assessment_dta), pattern="m5s2q")]))
 
 ####Total score####
 #calculate teachers percent correct
-teacher_assessment_dta <- teacher_assessment_dta %>%
+raw_teacher_assessment_dta <- raw_teacher_assessment_dta %>%
   mutate(content_knowledge=(math_content_knowledge+literacy_content_knowledge)/(literacy_length+math_length),
          math_content_knowledge=math_content_knowledge/math_length,
          literacy_content_knowledge=literacy_content_knowledge/literacy_length)
 
 
 #calculate % correct for literacy, math, and total
-school_content_knowledge <- teacher_assessment_dta %>%
+final_school_content_knowledge <- raw_teacher_assessment_dta %>%
   group_by(interview__id) %>%
   summarise(content_knowledge=mean(content_knowledge),
             math_content_knowledge=mean(math_content_knowledge),
@@ -206,7 +268,33 @@ school_student_knowledge <- assess_4th_grade_dta %>%
             literacy_student_knowledge=mean(literacy_student_knowledge))
 
 
+#############################################
+##### School Inputs ###########
+#############################################
 
+#functioning blackboard and chalk
+raw_school_data_INPT <- raw_school_data_INPT %>%
+  mutate(blackboard_functional=case_when(
+    blackboard_contrast=="Yes" & chalk=="Yes" & blackboard=="Yes"  ~ 1,
+    blackboard_contrast=="No" | chalk=="No" | blackboard=="No" ~ 0)) 
+
+#pens, pencils, textbooks, exercise books
+raw_school_data_INPT <- raw_school_data_INPT %>%
+  mutate(share_textbook=(books_boys+books_girls)/(num_boys+num_girls)) %>%
+  mutate(share_pencil=(pen_boys+pen_girls)/(num_boys+num_girls)) %>%
+  mutate(share_exbook=(booklet_boys+booklet_girls)/(num_boys+num_girls)) %>%
+  mutate(pens_etc=case_when(
+    share_textbook>=0.9 & share_pencil>=0.9 & share_exbook>=0.9  ~ 1,
+    share_textbook<0.9 | share_pencil<0.9 | share_exbook<0.9 ~ 0)) 
+
+#basic classroom furniture
+raw_school_data_INPT <- raw_school_data_INPT %>%
+  mutate(student_sit=if_else(student_sit=="Yes", 1,0, as.numeric(NA) ))
+
+#Used ICT
+
+
+#access to ICT
 
 
 #############################################
@@ -214,6 +302,8 @@ school_student_knowledge <- assess_4th_grade_dta %>%
 #############################################
 
 #Build school level database
+
+
 
 #merge on 4th grade student learning
 school_dta <- school_dta %>%
