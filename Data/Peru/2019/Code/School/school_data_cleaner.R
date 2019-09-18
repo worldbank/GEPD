105c93831e19989dd3cf5f0d7d213dd059d2f297
Brian Stacy <bstacy@worldbank.org>
2019-09-18 15:19:29 -0400

    updates to school indicators

--- Data/Peru/2019/Code/School/school_data_cleaner.R
+++ Data/Peru/2019/Code/School/school_data_cleaner.R
@@ -322,6 +322,20 @@ final_school_data_EFFT <- teacher_absence_dta %>%
 
 
 #############################################
+##### Student Attendance ###########
+#############################################
+
+#Percent of 4th grade students who are present during an unannounced visit.
+
+final_school_data_ATTD<- school_data_INPT %>%
+  select(preamble_info, m4scq4_inpt, m4scq4_inpt, m4scq12_inpt ) %>%
+  mutate(student_attendance=m4scq4_inpt/m4scq12_inpt) %>%
+  mutate(student_attendance=if_else(m4scq4_inpt>m4scq12_inpt, m4scq12_inpt/m4scq4_inpt,student_attendance))  %>% #fix an issue where sometimes enumerators will get these two questions mixed up.
+  group_by(school_code) %>%
+  summarise_all(~first(na.omit(.))) %>%
+  mutate(n_mssing_SSLD=n_miss_row(.)) 
+
+#############################################
 ##### Teacher Knowledge ###########
 #############################################
 
@@ -1166,6 +1180,11 @@ final_school_data_TEVL <- teacher_questionnaire_TEVL %>%
 ##### Teacher  Monitoring and Accountability ###########
 #############################################
 
+# School Survey. This policy lever measures the extent to which teacher presence is being monitored, whether attendance is rewarded, and whether there are consequences for chronic absence. Score is the sum of the following: 
+#   - 1 Point. Teachers evaluated by some authority on basis of absence. 
+# - 1.5 Points. Good attendance is rewarded. 
+# - 1.5 Points. There are consequences for chronic absence (more than 30% absence).
+
 teacher_questionnaire_TMNA2 <- teacher_questionnaire_TATT %>%
   select(interview__id, school_code, teacher_name, teacher_number, m3seq4_tatt, m3seq5_tatt__1, m3sbq1_other_tatt )
 
@@ -1209,6 +1228,11 @@ final_school_data_TMNA <- teacher_questionnaire_TMNA %>%
 ##### Teacher  Intrinsic Motivation ###########
 #############################################
 
+# 
+# School Survey. This lever measures whether teachers are intrinsically motivated to teach. The question(s) aim to address this 
+# phenomenon by measuring the level of intrinsic motivation among teachers as well as teacher values that may be relevant for 
+# ensuring that the teacher is motivated to focus on all children and not just some. Average score (1 (worst) - 5 (best)) on items 
+# given to teachers on intrinsic motivation.
 
 intrinsic_motiv_q_rev <- c('m3scq1_tinm','m3scq2_tinm', 'm3scq3_tinm', 'm3scq4_tinm', 'm3scq5_tinm', 'm3scq6_tinm',
                      'm3scq7_tinm', 'm3scq10_tinm')
@@ -1242,7 +1266,11 @@ final_school_data_TINM <- teacher_questionnaire_TINM %>%
 ##### School  Inputs and Infrastructure Monitoring ###########
 #############################################
 
-
+# School Survey. This lever measures the extent to which there is a monitoring system in place to ensure that the inputs that must be available at the schools are in fact available at the schools. This set of questions will include three aspects: 
+#   - 1 Point. Are there standards in place to monitor blackboard and chalk, pens and pencils, basic classroom furniture, computers, textbooks, exercise books, toilets, electricity, drinking water, accessibility for those with disabilities? (partial credit available) 
+# - 1 Point. Are all input items (functioning blackboard, chalk, pens, pencils, textbooks, exercise books in 4th grade classrooms, basic classroom furniture, and at least one computer in the schools) being monitored? (partial credit available) 
+# - 1 Point. Are all infrastructure items (functioning toilets, electricity, drinking water, and accessibility for people with disabilities) being monitored? (partial credit available) 
+# - 1 Point. Is the community involved in the monitoring?
 
 school_data_IMON <- school_data_IMON %>%
   mutate(standards_monitoring_input=rowSums(.[grep(x=colnames(school_data_IMON), 
@@ -1275,6 +1303,11 @@ final_school_data_IMON <- school_data_IMON %>%
 ##### School School Management Attraction  ###########
 #############################################
 
+# This policy lever measures whether the right candidates are being attracted to the profession of school principals. The questions will aim to capture the provision of benefits to attract and maintain the best people to serve as principals. 
+# 
+# Scoring: 
+#   - For now, score is between 1-5 based on how satisfied the principal is with status in community. We will also add in component based on Principal salaries.
+
 school_data_SATT <- school_data_SATT %>%
   mutate(principal_satisfaction=attitude_fun_rev(m7shq1_satt)) %>%
   mutate(school_management_attraction=principal_satisfaction)
@@ -1288,6 +1321,18 @@ final_school_data_SATT <- school_data_SATT %>%
 #############################################
 ##### School School Management Selection and Deployment  ###########
 #############################################
+
+
+# This policy lever measures whether the right candidates being selected. These questions will probe what the recruitment process is like to 
+# ensure that these individuals are getting the positions. The question would ultimately be based on: 1) there is a standard approach for selecting principals,
+# 2) that approach relies on professional/academic requirements, and 3) those requirements are common in practice. 
+# 
+# Scoring: 
+#   - 1 (lowest score) Most important factor is political affiliations or ethnic group. 
+# - 2 Political affiliations or ethnic group is a consideration, but other factors considered as well. 
+# - 3 Most important factor is years of experience, good relationship with owner/education department, and does not factor in quality teaching, demonstrated management qualities, or knowledge of local community. 
+# - 4 Quality teaching, demonstrated management qualities, or knowledge of local community is a consideration in hiring, but not the most important factor 
+# - 5 Quality teaching, demonstrated management qualities, or knowledge of local community is the most important factor in hiring.
 
 school_data_SSLD <- school_data_SSLD %>%
   mutate(school_selection_deployment=case_when(
@@ -1308,6 +1353,18 @@ final_school_data_SSLD <- school_data_SSLD %>%
 #############################################
 ##### School School Management Support  ###########
 #############################################
+
+
+# This policy lever measures the extent to which principals receive training and/or exposure to other professional opportunities that could help them be better school leaders. 
+# The questions aim to figure out if such programs are provided, and if they are, their level of quality. 
+# 
+# Scoring (sum of components below): 
+#   - 1 Point. Principal has received formal training on managing school. 
+# - 1/3 Point. Had management training for new principals. 
+# - 1/3 Point. Had management in-service training. 
+# - 1/3 Point. Had mentoring/coaching by experienced principals. 
+# - 1 Point. Have used skills gained at training. 
+# - 1 Point. Principals offered training at least once per year
 
 school_data_SSUP <- school_data_SSUP %>%
   mutate(prinicipal_trained=bin_var(m7sgq3_ssup,1),
@@ -1331,8 +1388,32 @@ final_school_data_SSUP <- school_data_SSUP %>%
 ##### School School Management Evaluation  ###########
 #############################################
 
+# School Survey. This policy lever measures the extent to which principal performance is being monitored and enforced via accountability measures. 
+# The idea is that the indicator will be based on: 1) there is a legislation outlining the need to monitor, 2) principals are being evaluated, 3) 
+# principals are being evaluated on multiple things, and 4) there the accountability mechanisms in place.
 
 
+
+school_data_SEVL<- school_data_SEVL %>%
+  mutate(principal_formally_evaluated=bin_var(m7sgq8_sevl,1),
+         principal_eval_tot=rowSums(.[grep(x=colnames(school_data_SEVL), 
+                                           pattern="m7sgq10_sevl__")], na.rm=TRUE)-m7sgq10_sevl__98) %>%
+  mutate(principal_evaluation_multiple=if_else(m7sgq8_sevl==1,
+                                            case_when(
+                                              principal_eval_tot>=5 ~ 1,
+                                              (principal_eval_tot>1 & principal_eval_tot<5) ~ 0.666667,
+                                              principal_eval_tot==1 ~ 0.3333333,
+                                              TRUE ~ 0
+                                            ),
+                                            0),
+         principal_negative_consequences=case_when(
+           (m7sgq11_sevl__1==1 | m7sgq11_sevl__2==1 | m7sgq11_sevl__3==1 | m7sgq11_sevl__4==1 | m7sgq11_sevl__97==1) ~ 1,
+           TRUE ~ 0),
+         principal_positive_consequences=case_when(
+           (m7sgq12_sevl__1==1 | m7sgq12_sevl__2==1 | m7sgq12_sevl__3==1 | m7sgq12_sevl__4==1 | m7sgq12_sevl__97==1) ~ 1,
+           TRUE ~ 0)
+  ) %>%
+  mutate(principal_evaluation=1+principal_formally_evaluated+principal_evaluation_multiple+principal_negative_consequences+principal_positive_consequences)
 
 final_school_data_SEVL <- school_data_SEVL %>%
   group_by(school_code) %>%
@@ -1386,14 +1467,14 @@ for (i in indicator_names ) {
 
 #Create list of key indicators
 ind_list<-c('student_knowledge', 'math_student_knowledge', 'literacy_student_knowledge',
-            'absence_rate', 'school_absence_rate', 
+            'absence_rate', 'school_absence_rate', 'student_attendance',
             'content_knowledge', 'math_content_knowledge', 'literacy_content_knowledge',
             'ecd_student_knowledge', 'ecd_math_student_knowledge', 'ecd_literacy_student_knowledge', 'ecd_exec_student_knowledge', 'ecd_soc_student_knowledge',
             'inputs', 'blackboard_functional', 'pens_etc', 'share_desk', 'used_ict', 'access_ict',
             'infrastructure','disab_road_access', 'disab_school_ramp', 'disab_school_entr', 'disab_class_ramp', 'disab_class_entr', 'disab_screening',
             'operational_management', 'vignette_1', 'vignette_2', 'intrinsic_motivation', 'instructional_leadership','principal_management','teacher_attraction',
             'teacher_selection_deployment', 'teacher_support', 'teaching_evaluation', 'teacher_monitoring','school_monitoring',
-            'school_management_attraction', 'school_selection_deployment', 'school_support'
+            'school_management_attraction', 'school_selection_deployment', 'school_support', 'principal_evaluation'
 )
 
 
