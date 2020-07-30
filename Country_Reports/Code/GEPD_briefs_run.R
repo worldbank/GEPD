ecfd83e01d4c8e5556bcdc4ead79bafe284f25f7
Brian Stacy <stacybw@gmail.com>
2020-07-30 10:41:48 -0400

    add tweaks to handle key takeaways

--- Country_Reports/Code/GEPD_briefs_run.R
+++ Country_Reports/Code/GEPD_briefs_run.R
@@ -31,6 +31,15 @@ country <- "Peru"
 country_uc <- "PERU"
 year <- "2019"
 
+
+takeaway <- "
+- Learning poverty is substantial, and rural children lag far beyond urban children in learning.
+- Disparities are driven by differences in practices (less than ½ of variation explained by within-school differences).
+- Of all practice indicators, teacher skills & children’s capacity for learning at primary entry explain low learning the best.
+- Infrastructure is advanced, but internet access and accessibility to kids with disabilities are still poor (just 8% & 54% in rural schools).
+- Policy frameworks are comprehensive, but the de facto implementation varies across types of policies – E.g. teaching support.
+- Bureaucratic capacity scores are affected by the lack of recognition of good performance for individuals, units, and departments. 
+"
 # 2 Pager
 rmarkdown::render('./Code/GEPD_Brief_2page.Rmd',  
                   output_file =  paste(country,"_", year,"_", "2Pager.pdf", sep=''), 
@@ -51,6 +60,15 @@ country_file_name <- "JOR"
 country <- "Jordan"
 country_uc <- "JORDAN"
 year <- "2019"
+
+takeaway <- "
+- 52% learning poverty is observed at grade 4 level
+- Overall Grade 4 student proficiency (>80% student knowledge) is only 4%, attributed to low numeracy proficiency 2%).
+- Low teacher content knowledge attributed to poor teacher support and lack of strong instructional leadership (especially principal feedback on classroom observation)
+- Low capacity for learning in Grade 1 (particularly socio-emotional knowledge) is attributed to low enrolment in early childhood programs (13%)
+- A ~0.8 point difference exists in the policy de-jure and de-facto indicators on a 5 point scale for teaching and school management. Major gaps are seen in teacher support, teacher motivation and evaluation of school management
+- Bureaucracy scores high on mandates and accountability but low on impartial decision making
+"
 
 # 2 Pager
 rmarkdown::render('./Code/GEPD_Brief_2page.Rmd',  
