2884ba608a9234502c25ea1c773485d2f802f11d
Brian Stacy <stacybw@gmail.com>
2020-12-22 15:58:29 -0500

    add diagram and change paths

--- Data/Peru/2019/Code/School/01_data/01_school_run.R
+++ Data/Peru/2019/Code/School/01_data/01_school_run.R
@@ -38,9 +38,9 @@ year <- '2019'
 
 
 
-if (Sys.getenv("USERNAME") == "wb469649"){
+if (Sys.getenv("USERNAME") == "WB469649" | Sys.getenv("USERNAME") == "wb469649"){
   #project_folder  <- "//wbgfscifs01/GEDEDU/datalib-edu/projects/gepd"
-  project_folder  <- "//wbgfscifs01/GEDEDU/datalib-edu/projects/GEPD-Confidential/CNT/"
+  project_folder  <- "C:/Users/wb469649/WBG/HEDGE Files - HEDGE Documents/GEPD-Confidential/CNT/"
   download_folder <-file.path(paste(project_folder,country,paste(country,year,"GEPD", sep="_"),paste(country,year,"GEPD_v01_RAW", sep="_"),"Data/raw/School", sep="/"))
   confidential_folder <- file.path(paste(project_folder,country,paste(country,year,"GEPD", sep="_"),paste(country,year,"GEPD_v01_RAW", sep="_"),"Data/confidential/School", sep="/"))
   save_folder <- file.path(paste(project_folder,country,paste(country,year,"GEPD", sep="_"),paste(country,year,"GEPD_v01_RAW", sep="_"),"Data/anonymized/School", sep="/"))
@@ -53,7 +53,6 @@ if (Sys.getenv("USERNAME") == "wb469649"){
   save_folder <- choose.dir(default = "", caption = "Select folder to save final data")
 
 }
-
 
 
 #########################
