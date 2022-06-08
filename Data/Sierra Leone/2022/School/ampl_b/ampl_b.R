f11a7ed53e297a899225d5c6a24105741006514d
Adrien Ciret <adri.ciret@gmail.com>
2022-06-08 17:03:44 -0400

    AMPL-b cleaning

--- Data/Sierra Leone/2022/School/ampl_b/ampl_b.R
+++ Data/Sierra Leone/2022/School/ampl_b/ampl_b.R
@@ -79,6 +79,23 @@ amplb<-read_dta(file.path(download_folder, "ampl_dash_v3.dta")) %>%
 roster <- read_dta(file.path(download_folder, "questionnaire_roster.dta")) %>% 
   bind_rows(read_dta(file.path(download_folder, "questionnaire_roster_v3.dta")))
 
+
+
+
+
+
+## Correct the issue with labelling
+library(Hmisc)
+label(roster[["Item2_1"]]) <- "MR004"
+label(roster[["Item32_2"]]) <- "MR004"
+label(roster[["Item52_1"]]) <- "MM089"
+label(roster[["Item22_2"]]) <- "MM089"
+# label(roster[["Item11_2"]]) <- "MM175"
+# label(roster[["Item41_1"]]) <- "MM175"
+
+
+## Metadata
+
 meta_amplb <- makeVlist(roster) %>% select(-c(vallabel)) %>% rename(`Item number new`=name) %>% 
   filter(str_detect(`Item number new`, "^Item")) %>% 
   mutate(`Item number new` = gsub("(.*)_(.*)", "\\1", `Item number new`))
@@ -88,7 +105,8 @@ ampl_b <- roster %>%
   select(-m3sb_troster) %>%
   left_join(amplb %>% select(interview__key, ends_with("preload"))) %>% 
   mutate(unique_id = as.character(lapply(student_name, function(x) {digest(x, algo="xxhash64", seed=531254, serialize = T)}))) %>% 
-  relocate(ends_with("preload"))
+  relocate(ends_with("preload")) %>% 
+  mutate(unique_id= paste0(unique_id, interview__id))
 
 preamble <- ampl_b %>% select(1:15, unique_id, starts_with("Item7")) %>% label_to_colnames(starts_with("Item7"))
 
@@ -102,9 +120,26 @@ ampl_b <- ampl_b %>%
 #write.csv(ampl_b, "C:/Users/wb577189/OneDrive - WBG/Desktop/amplb_batch_1_sl.csv", row.names = F, na = "")
 # write_dta(ampl_b, "C:/Users/wb577189/OneDrive - WBG/Desktop/amplb_batch_1_sl.dta")
 
+## Item MM175 was a complex MC question, so we clean it and gather the response into one var
+MM175 <- ampl_b %>%
+  unite("Item11_2", Item11_2_1:Item11_2_4,  sep= "",
+        remove = FALSE) %>%
+  unite("Item41_1", Item41_1_1:Item41_1_4,  sep= "",
+        remove = FALSE) %>%
+  mutate_all(funs(str_replace(., "NANANANA", NA_character_)))  %>% select(unique_id, Item11_2, Item41_1) %>% 
+  mutate(across(c(Item11_2, Item41_1), ~ as.numeric(.))) %>% 
+  
+  mutate(MM175 = coalesce(Item11_2, Item41_1)) %>% select(unique_id, MM175)
+
+
+label(MM175[["MM175"]]) <- "MM175"
+
+
+
 ## first we pivot from wide to long
 
 roster_long <- roster %>%
+  mutate(across(c(Item1_1:Item76), ~ as.character(.))) %>% 
   pivot_longer(
     cols = starts_with("Item"),
     names_to = c("Item number", "Booklet type"),
@@ -134,7 +169,7 @@ roster_long <- roster %>%
 
 ## Then we re order the question so that booklets 1 and 2 have literacy and numeracy items in the same order
   
-  
+ meta <-  labelled::generate_dictionary(ampl_b)
   
 item_fun<- function(var){
   
@@ -142,11 +177,14 @@ item_fun<- function(var){
     
   x <- ampl_b %>% 
     select(unique_id, items) %>% 
-    mutate(across(items, ~ as.numeric(.))) %>% 
     select(items) %>% 
+    ## Have to remove the labels otherwise we cannot convert to numeric
+    labelled::remove_labels() %>% 
+    mutate(across(items, ~ as.numeric(.))) %>% 
+    
     mutate(var = coalesce(!!!.)) %>% bind_cols(ampl_b %>% select(unique_id)) %>%  select(unique_id, var) 
   
-  names(x)[2] <-  quo_name(enquo(var))
+    names(x)[2] <-  quo_name(enquo(var))
     
    assign(paste0("item_", quo_name(enquo(var))), x, envir = .GlobalEnv)
     
@@ -166,16 +204,20 @@ df_list <- mget(ls(pattern = "^item_[A-Z].*"))
 final <- df_list %>% reduce(full_join, by = "unique_id")%>%
   select(unique_id, everything()) %>% 
   left_join(preamble %>% select(-MM208, -MR003)) %>% 
-  relocate(names(preamble))
+  ## Add complex MM175
+  left_join(MM175) %>% 
+  relocate(names(preamble)) %>% 
+  distinct(unique_id, .keep_all=T)
 
+n_distinct(final$unique_id)
 
 
 ## Save the data
 write_dta(final %>% select(-student_name), file.path(save_folder, "amplb_sl.dta"))
-write.csv(final %>% select(-student_name), file.path(save_folder, "amplb_sl.csv"))
+write.csv(final %>% select(-student_name), file.path(save_folder, "amplb_sl.csv"), row.names = F)
 
 write_dta(final, file.path(save_folder, "amplb_key_confidential_sl.dta"))
-write.csv(final, file.path(save_folder, "amplb_key_confidential_sl.csv"))
+write.csv(final, file.path(save_folder, "amplb_key_confidential_sl.csv"), row.names = F)
 
 
 
