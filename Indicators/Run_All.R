9c811a9da20eac0408ef1099cf15df04a7553e29
Brian Stacy <stacybw@gmail.com>
2025-05-08 19:49:57 -0400

    update to indicator file

--- Indicators/Run_All.R
+++ Indicators/Run_All.R
@@ -13,14 +13,14 @@ files <- list.files(pattern = "indicator_api_final")
 
 #manual
 files <- c(
-  #"indicator_api_final.R" ,
-  #"indicator_api_final_Ethiopia.R" ,
-  #"indicator_api_final_GAB.R"  , 
-  "indicator_api_final_Jordan.R"   ,   
-  "indicator_api_final_Madagascar.R",
-  "indicator_api_final_NER.R"      ,   
-  "indicator_api_final_Rwanda.R"   ,  
-  "indicator_api_final_SLE.R"    
+  "indicator_api_final.R" ,
+  "indicator_api_final_Ethiopia.R" ,
+  "indicator_api_final_GAB.R"  , 
+  "indicator_api_final_Jordan.R"     ,    
+  "indicator_api_final_SLE.R"    ,
+  "indicator_api_final_Madagascar.R",   
+  "indicator_api_final_Rwanda.R"   , 
+  "indicator_api_final_NER.R"    
 )
 
 #run all files
